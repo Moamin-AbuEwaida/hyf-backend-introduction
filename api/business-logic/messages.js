17ef48468afd875f43521c59126d3726f9bedba4
Moamin-AbuEwaida <moamin.salamah@gmail.com>
2021-10-15 11:03:34 +0200

    working branch

--- api/business-logic/messages.js
+++ api/business-logic/messages.js
@@ -7,21 +7,55 @@ const messageStore = persistentDataAccess('messages');
 const messageManager = {
   createMessage: async (user, messageContent, channelId) => {
     // TODO: implement
+    const id = objectId().toString();
+    const message = {
+      text: messageContent,
+      id: id,
+      user: user,
+      channelId: channelId,
+      date: new Date(),
+    };
+    await messageStore.create(message);
+    return message;
   },
   updateMessage: async (message) => {
     // TODO: implement
+    const newMessage = await messageStore.update(message.id, message);
+    if (!success) {
+      throw new Error('Cannot update message');
+    }
+    return newMessage;
   },
   removeMessage: async (messageId) => {
     // TODO: implement
+    await messageStore.remove(messageId);
+    return true;
   },
   getMessage: async (messageId) => {
     // TODO: implement
+    const message = await messageStore.read(messageId);
+
+    if (!message) {
+      throw new Error(`couldn't find the message!`);
+    }
+
+    return message;
   },
   getAllMessages: async () => {
     // TODO: implement
+    return await messageStore.all();
   },
   getMessagesForChannel: async (channelId) => {
     // TODO: implement
+    const res = [];
+    const allMessages = await messageStore.all(channelId);
+    for (let i = 0; i < allMessages.length; i++) {
+      const theMessage = allMessages[i];
+      if (theMessage.channelId === channelId) {
+        res.push(theMessage);
+      }
+    }
+    return res;
   },
 };
 
--- api/controllers/messages.js
+++ api/controllers/messages.js
@@ -4,30 +4,74 @@ const messageController = {
   get: async (req, res) => {
     // returns all messages currently in the system
     // TODO implement
-    res.send(JSON.stringify([]));
+    try {
+      const messages = await messageManager.getAllMessages();
+      // console.log(messages);
+      res.status(200).send(JSON.stringify(messages));
+    } catch (error) {
+      res.status(500).send(error);
+    }
   },
   getMessagesForChannel: async (req, res) => {
     // returns the messages that belong in the channel with the specified id
     // passed as /api/channels/:channelId/messages
     // TODO implement
-    res.send(JSON.stringify([]));
+    try {
+      const channelId = req.params.channelId;
+      // console.log(channelId);
+      const message = await messageManager.getMessagesForChannel(channelId);
+      // console.log(message);
+      res.status(200).send(JSON.stringify(message));
+    } catch (error) {
+      res.status(500).send(error);
+    }
   },
   put: async (req, res) => {
     // updates the messages with the specified id
     // passed as /api/messages/:messageId
     // TODO implement
-    res.send('Not yet implemented');
+    //res.send('Not yet implemented');
+    try {
+      const messageId = req.params.messageId;
+      // console.log(messageId);
+      const newMessage = req.body;
+      if (newMessage.id !== messageId) {
+        throw Error('cannot edit a un existed message!');
+      }
+      res.status(200).send(JSON.stringify(newMessage));
+    } catch (error) {
+      res.status(500).send(error);
+    }
   },
   post: async (req, res) => {
     // creates a new message based on the passed body
     // TODO implement
-    res.send('Not yet implemented');
+    //res.send('Not yet implemented');
+    try {
+      const user = req.body.user;
+      const content = req.body.text;
+      const channelId = req.params.channelId;
+      const message = await messageManager.createMessage(
+        user,
+        content,
+        channelId,
+      );
+      res.status(200).send(JSON.stringify(message));
+    } catch (error) {
+      res.status(500).send(error);
+    }
   },
   delete: async (req, res) => {
     // deleted the message with the specified id
     // passed as /api/messages/:messageId
     // TODO implement
-    res.send('Not yet implemented');
+    try {
+      const messageId = req.params.messageId;
+      await messageManager.removeMessage(messageId);
+      res.status(200).send({ message: `message id ${messagId} was deleted!` });
+    } catch (error) {
+      res.status(500).send(error);
+    }
   },
 };
 
